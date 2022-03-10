aa5dea8d4e600ffe81d4f3edf35eec377acff32a
EdgarOsorio <edgar.zh.osorio@gmail.com>
2022-03-09 18:12:36 -0600

    Created html layout and added styles to Formulario & ListadoPacientes

---
+++ src/components/Paciente.jsx
@@ -0,0 +1,23 @@
+const Paciente = () => {
+    return (
+        <div className="m-3 bg-white shadow-md px-5 py-10 rounded-xl">
+            <p className="font-bold mb-3 text-gray-700 uppercase">Nombre: {''}
+                <span className="font-normal normal-case">Hook</span>
+            </p>
+            <p className="font-bold mb-3 text-gray-700 uppercase">Propietario: {''}
+                <span className="font-normal normal-case">Edgar</span>
+            </p>
+            <p className="font-bold mb-3 text-gray-700 uppercase">Email: {''}
+                <span className="font-normal normal-case">correo@correo.com</span>
+            </p>
+            <p className="font-bold mb-3 text-gray-700 uppercase">Fecha Alta: {''}
+                <span className="font-normal normal-case">10/01/22</span>
+            </p>
+            <p className="font-bold mb-3 text-gray-700 uppercase">Sintomas: {''}
+                <span className="font-normal normal-case">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Provident, architecto quibusdam. Perspiciatis sint praesentium autem doloribus accusantium corporis. Dolor aliquid dolorem vitae voluptates molestiae quo dicta dignissimos impedit nemo qui!</span>
+            </p>
+        </div>
+    )
+}
+
+export default Paciente
