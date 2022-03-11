0df1f2d85e1178a6c0ab00d5489c37ff02a4f16c
EdgarOsorio <edgar.zh.osorio@gmail.com>
2022-03-10 19:39:00 -0600

    Added props to Formulario component

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,16 +1,42 @@
 import { useState, useEffect } from "react"
 
-const Formulario = () => {
+const Formulario = ({pacientes, setPacientes}) => {
 
   const [nombre, setNombre] = useState('')
   const [propietario, setPropietario] = useState('')
   const [email, setEmail] = useState('')
   const [fecha, setFecha] = useState('')
   const [sintomas, setSintomas] = useState('')
+  
+  const [error, setError] = useState(false)
 
   const handleSubmit = (e) => {
     e.preventDefault()
-    console.log('Enviando')
+    
+    // Validacion del formulario
+    if([nombre, propietario, email, fecha, sintomas].includes('')) {
+      console.log('Hay almenos un campo vacio')
+      setError(true)
+      return
+    }
+    setError(false)
+
+    //Objeto de Paciente
+    const objetoPaciente = {
+      nombre, 
+      propietario, 
+      email, 
+      fecha, 
+      sintomas
+    }
+    setPacientes([...pacientes, objetoPaciente])
+
+    // Reiniciar formulario
+    setNombre('')
+    setPropietario('')
+    setEmail('')
+    setFecha('')
+    setSintomas('')
   }
 
   return (
@@ -24,6 +50,11 @@ const Formulario = () => {
           className="bg-white shadow-md rounded-lg py-10 px-5 mb-10 mx-5"
           onSubmit={handleSubmit}
         >
+          {error && (
+            <div className="bg-red-800 text-white text-center p-3 uppercase font-bold mb-3 rounded">
+              <p>Todos los campos son obligatorios</p>
+            </div>
+          ) }
           <div className="mb-5">
             <label 
               htmlFor="mascota" 
